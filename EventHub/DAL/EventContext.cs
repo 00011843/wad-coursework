5bcdddc2bc196eddf7aea68f5bfbfe88c08929b3
jaykhans <jahongirnagmatov@gmail.com>
2023-03-14 11:23:29 +0500

    Add Event dbContext to connect with database using EF core

---
+++ EventHub/DAL/EventContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EventHub.Models;
+
+namespace EventHub.DAL
+{
+    public class EventContext: DbContext
+    {
+        public EventContext(DbContextOptions<EventContext> o) : base(o) 
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}

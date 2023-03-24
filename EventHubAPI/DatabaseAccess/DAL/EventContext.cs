4c30983d13480f397e84927223766c70ba58e05c
jaykhans <jahongirnagmatov@gmail.com>
2023-03-24 11:42:15 +0500

    Remove old project and create API project with Database Access layer

---
+++ EventHubAPI/DatabaseAccess/DAL/EventContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EventHubAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventHubAPI.DAL
+{
+    public class EventContext: DbContext
+    {
+        public EventContext(DbContextOptions<EventContext> options) : base(options) 
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Category>  Categories { get; set; }
+        public DbSet<Event> Events { get;set; }
+    }
+}
---
+++ EventHubAPI/DatabaseAccess/Repositories/CategoryRepository.cs
@@ -0,0 +1,55 @@
+﻿using DatabaseAccess.DAL;
+using DatabaseAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DatabaseAccess.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly EventContext __dbContext;
+
+        public CategoryRepository(EventContext dbContext)
+        {
+            __dbContext = dbContext;
+        }
+
+        public void Save()
+        {
+            __dbContext.SaveChanges();
+        }
+
+        public void CreateNewCategory(Category category)
+        {
+            __dbContext.Add(category);
+            Save();
+        }
+
+        public void DeleteCategoryById(int CategoryId)
+        {
+            var foundCategory = __dbContext.Categories.Find(CategoryId);
+            __dbContext.Categories.Remove(foundCategory);
+            Save();
+        }
+
+        public IEnumerable<Category> GetAllCategories()
+        {
+            return __dbContext.Categories.ToList();
+        }
+
+        public Category GetCategoryById(int CategoryId)
+        {
+            return __dbContext.Categories.Find(CategoryId);
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            //Modify the category through EntityFrameworCore EnttiyState module
+            __dbContext.Entry(category).State = EntityState.Modified;
+            Save();
+        }
+    }
+}
